98b8a4826420175e1f043200ef568aa6dfd5b2b6
Erol Fornoles <erol.fornoles@gmail.com>
2015-02-03 08:08:30 +0800

    Add Ethos::Type

---
+++ lib/ethos/type.rb
@@ -0,0 +1,61 @@
+require 'date'
+require 'time'
+require 'bigdecimal'
+
+module Ethos
+  module Type
+    CASTS = {
+      nil => lambda { |value| value }
+    }
+
+    def self.define(type, &block)
+      CASTS[type] = block
+    end
+
+    def self.cast(value, type)
+      return value if value.is_a? type
+
+      CASTS[type][value]
+    end
+  end
+end
+
+Ethos::Type.define Symbol do |value|
+  value.to_sym
+end
+
+Ethos::Type.define String do |value|
+  String value
+end
+
+Ethos::Type.define Integer do |value|
+  Integer value
+end
+
+Ethos::Type.define Float do |value|
+  Float value
+end
+
+Ethos::Type.define Date do |value|
+  Date.parse value
+end
+
+Ethos::Type.define Time do |value|
+  Time.parse value
+end
+
+Ethos::Type.define DateTime do |value|
+  DateTime.parse value
+end
+
+Ethos::Type.define Hash do |values|
+  Hash[*values]
+end
+
+Ethos::Type.define Array do |values|
+  Array[*values]
+end
+
+Ethos::Type.define BigDecimal do |value|
+  BigDecimal.new String value
+end
---
+++ spec/ethos/type.rb
@@ -0,0 +1,73 @@
+require 'ethos/type'
+
+scope '.cast' do
+  spec 'casts a value to a Symbol' do
+    expected = :Symbol
+    result = Ethos::Type.cast 'Symbol', Symbol
+
+    asserts(result) == expected
+  end
+
+  spec 'casts a value to a String' do
+    expected = String 1
+    result = Ethos::Type.cast 1, String
+
+    asserts(result) == expected
+  end
+
+  spec 'casts a value to an Integer' do
+    expected = Integer 1
+    result = Ethos::Type.cast 1, Integer
+
+    asserts(result) == expected
+  end
+
+  spec 'casts a value to a Float' do
+    expected = Float 1
+    result = Ethos::Type.cast 1, Float
+
+    asserts(result) == expected
+  end
+
+  spec 'casts a value to a Date' do
+    expected = Date.parse '2014-01-01'
+    result = Ethos::Type.cast '2014-01-01', Date
+
+    asserts(result) == expected
+  end
+
+  spec 'casts a value to a Time' do
+    expected = Time.parse '2014-01-01 12:00AM'
+    result = Ethos::Type.cast '2014-01-01 12:00AM', Time
+
+    asserts(result) == expected
+  end
+
+  spec 'casts a value to a DateTime' do
+    expected = DateTime.parse '2014-01-01 12:00AM'
+    result = Ethos::Type.cast '2014-01-01 12:00AM', DateTime
+
+    asserts(result) == expected
+  end
+
+  spec 'casts a value to a Hash' do
+    expected = Hash[:a, 1]
+    result = Ethos::Type.cast [:a, 1], Hash
+
+    asserts(result) == expected
+  end
+
+  spec 'casts a value to a Array' do
+    expected = Array[1, 2]
+    result = Ethos::Type.cast [1, 2], Array
+
+    asserts(result) == expected
+  end
+
+  spec 'casts a value to a BigDecimal' do
+    expected = BigDecimal.new '10.10'
+    result = Ethos::Type.cast 10.10, BigDecimal
+
+    asserts(result) == expected
+  end
+end
