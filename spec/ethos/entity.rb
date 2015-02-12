7a20575f9aa124ce2ef2c71ec97fc38286b1cd4b
Erol Fornoles <erol.fornoles@gmail.com>
2015-02-12 08:43:51 +0800

    Test embedding of entities as attributes

--- spec/ethos/entity.rb
+++ spec/ethos/entity.rb
@@ -59,3 +59,34 @@ scope do
     asserts(entity.value) == 1
   end
 end
+
+scope do
+  setup do
+    class Entity
+      prepend Ethos::Entity
+
+      attribute :name, type: String
+      attribute :parent, type: Entity
+    end
+  end
+
+  scope do
+    define entity: -> { Entity.new }
+
+    spec do
+      asserts(entity.parent) == nil
+    end
+  end
+
+  scope do
+    define entity: -> { Entity.new name: 'Child', parent: {name: 'Parent'} }
+
+    spec do
+      asserts(entity.parent).is_a? Entity
+    end
+
+    spec do
+      asserts(entity.parent.name) == 'Parent'
+    end
+  end
+end
